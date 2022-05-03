0dbe07c08d88f1ae907ec72914a42ac9ead62536
pupixipup <rkbitinas@gmail.com>
2022-05-03 11:25:47 +0200

    feat: centre the canvas

--- farming-game.js
+++ farming-game.js
@@ -1,5 +1,11 @@
+let grass;
+
+function preload() {
+    grass = loadImage('assets/grass.png');
+}
 function setup() {
     let canvas = createCanvas(1280, 720);
     background('red');
+    image(grass, 0, 0);
     canvas.parent('container');
 }
