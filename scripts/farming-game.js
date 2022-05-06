6a6082c6b6e8d175d36cf0761f63665caa39bd58
pupixipup <rkbitinas@gmail.com>
2022-05-06 22:08:05 +0200

    feat: add second player

--- scripts/farming-game.js
+++ scripts/farming-game.js
@@ -5,12 +5,17 @@ import { Garden } from "./garden.js";
 let sketch = new p5();
 let grass;
 let animationTopPlayerOne;
+let animationTopPlayerTwo;
 
 function preload() {
   grass = loadImage("assets/grass.png");
   animationTopPlayerOne = [
     loadImage("assets/blue-man-running1.png"),
     loadImage("assets/blue-man-running2.png"),
+  ];
+  animationTopPlayerTwo = [
+    loadImage("assets/red-man-run1.png"),
+    loadImage("../assets/red-man-run2.png"),
   ];
 }
 
@@ -26,11 +31,15 @@ window.setup = setup;
 let index = 0;
 // You can see what do these keys stand for in player.js
 let PlayerOne = new Player(500, 500, 68, 3, 65, 87, 83);
+let PlayerTwo = new Player(100, 100, 39, 3, 37, 38, 40);
 function draw() {
   image(grass, 0, 0);
   let tiles = new Garden();
   tiles.drawTiles(14, 9, 60);
   PlayerOne.animatePlayer(animationTopPlayerOne, index);
+  PlayerTwo.animatePlayer(animationTopPlayerTwo, index);
+
+  PlayerTwo.movePlayer();
   PlayerOne.movePlayer();
 
 // -- 
